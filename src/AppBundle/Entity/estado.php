a054f6b15cd38360810a4169ceb3607e4440203b
juanluisparra <juanluis.parrarus@hotmail.com>
2016-12-13 17:42:50 +0100

    añadido entidad estado

---
+++ src/AppBundle/Entity/estado.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * estado
+ *
+ * @ORM\Table(name="estado")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\estadoRepository")
+ */
+class estado
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="condicion", type="string", length=255)
+     */
+    private $condicion;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set condicion
+     *
+     * @param string $condicion
+     *
+     * @return estado
+     */
+    public function setCondicion($condicion)
+    {
+        $this->condicion = $condicion;
+
+        return $this;
+    }
+
+    /**
+     * Get condicion
+     *
+     * @return string
+     */
+    public function getCondicion()
+    {
+        return $this->condicion;
+    }
+}
+
