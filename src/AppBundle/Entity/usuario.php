97f99949a295bf63b4a106acc1630a5a6e6af802
juanluisparra <juanluis.parrarus@hotmail.com>
2016-12-13 17:39:21 +0100

    añadido entidad usuario

---
+++ src/AppBundle/Entity/usuario.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * usuario
+ *
+ * @ORM\Table(name="usuario")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\usuarioRepository")
+ */
+class usuario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellido", type="string", length=255)
+     */
+    private $apellido;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="correo", type="string", length=255)
+     */
+    private $correo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contraseña", type="string", length=255)
+     */
+    private $contraseña;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="responsable", type="boolean")
+     */
+    private $responsable;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellido
+     *
+     * @param string $apellido
+     *
+     * @return usuario
+     */
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    /**
+     * Get apellido
+     *
+     * @return string
+     */
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * Set correo
+     *
+     * @param string $correo
+     *
+     * @return usuario
+     */
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    /**
+     * Get correo
+     *
+     * @return string
+     */
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    /**
+     * Set contraseña
+     *
+     * @param string $contraseña
+     *
+     * @return usuario
+     */
+    public function setContraseña($contraseña)
+    {
+        $this->contraseña = $contraseña;
+
+        return $this;
+    }
+
+    /**
+     * Get contraseña
+     *
+     * @return string
+     */
+    public function getContraseña()
+    {
+        return $this->contraseña;
+    }
+
+    /**
+     * Set responsable
+     *
+     * @param boolean $responsable
+     *
+     * @return usuario
+     */
+    public function setResponsable($responsable)
+    {
+        $this->responsable = $responsable;
+
+        return $this;
+    }
+
+    /**
+     * Get responsable
+     *
+     * @return bool
+     */
+    public function getResponsable()
+    {
+        return $this->responsable;
+    }
+}
+
