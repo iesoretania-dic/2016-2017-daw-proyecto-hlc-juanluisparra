62e358bffaf6a7b085a68c142b6d26eba917271f
juanluisparra <juanluis.parrarus@hotmail.com>
2017-03-06 19:05:43 +0100

    listado de locales

--- src/AppBundle/Controller/LocalController.php
+++ src/AppBundle/Controller/LocalController.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: juan
- * Date: 6/03/17
- * Time: 16:51
- */
+namespace AppBundle\Controller;
+
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LocalController extends Controller
+{
+    /**
+     * @Route("/locales", name="listar_locales")
+     */
+    public function indexAction()
+    {
+
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $locales = $em->createQueryBuilder()
+            ->select('l')
+            ->from('AppBundle:local','l')
+            ->getQuery()
+            ->getResult();
+        return $this->render('local/listar.html.twig', [
+            'locales' => $locales
+        ]);
+    }
+}
